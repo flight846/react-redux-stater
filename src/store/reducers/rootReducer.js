7210d442ddb12f750796a8ca2248f877b05fb276
yazid <yazid@decision-science.com>
2019-02-13 23:54:26 +0800

    Init redux

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    logo: '../src/logo.svg',
+    code: 'src/App.js',
+    appLink: 'https://reactjs.org'
+}
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default rootReducer;
